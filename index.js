7a72b9b5c9e25a753f32602f3ad479a04c39cea7
Whotislove <vlad.lobunets@mail.ru>
2022-10-31 17:15:29 +0300

    fix

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ import * as ItemController from './controllers/ItemController.js';
 import cors from 'cors';
 mongoose
   .connect(
-    /*process.env
-      .MONGODB_URI*/ 'mongodb+srv://admin:wwwwww@cluster0.lmxnk7s.mongodb.net/collection?retryWrites=true&w=majority',
+    process.env
+      .MONGODB_URI /*'mongodb+srv://admin:wwwwww@cluster0.lmxnk7s.mongodb.net/collection?retryWrites=true&w=majority'*/,
   )
   .then(() => {
     console.log('DB OK');
